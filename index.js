90cc3b6a2af68710d95ae2560b0598fdd1edcad7
Vasanth <sayhello@vasanthsriram.com>
2015-10-03 16:48:40 +0530

    command line argument works

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+#! /usr/bin/env node
+
 var main  = require('./lib/main.js');
+var jade = require('jade');
+var fs = require('fs');
+var path = require('path');
 
 main.doNPMManifest(process.cwd());
 main.doBowerManifest(process.cwd());
 
-console.log(main.getManifest());
+var allData = main.getManifest();
+
+var manifestData = {
+	app: allData.slice(0, 1)[0],
+	deps: allData.slice(1, allData.length)
+}
+
+var report = jade.renderFile(path.join(__dirname, './report/index.jade'), manifestData);
+
+fs.writeFileSync(path.join(process.cwd(), 'manifest.html'), report);
